6cd6b73c9b020f4dac01c18faae1031c29dd9b4d
harshadasawant22@gmail.com <Panasonic@1209>
2023-07-31 13:26:38 +0530

    first commit

---
+++ sample/src/Functions.java
@@ -0,0 +1,27 @@
+import java.util.function.Function;
+
+public class Functions {
+
+    public void showExample() {
+        System.out.println("I am called from function");
+    }
+
+    public int showExample(int a) {
+        System.out.println("I am called from function a = " + a);
+        return a;
+    }
+
+    public double showExample(float a) {
+        System.out.println("I am called from function a = " + a);
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Functions obj1 = new Functions();
+        obj1.showExample();
+        int a = obj1.showExample(6);
+        double d = obj1.showExample(6.7F);
+        System.out.println(a + d);
+
+    }
+}
